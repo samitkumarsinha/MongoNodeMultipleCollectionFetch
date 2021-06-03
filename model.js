fbd2cb51acc16c8b22986e92a8a4de18c601ad47
samitkumarsinha <samitkumarsinha@gmail.com>
2021-06-03 16:13:03 +0530

    initial commit

--- model.js
+++ model.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 var uri = "mongodb://127.0.0.1:27017/blog";
-
 module.exports = {
     getuserdata(){
         mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
