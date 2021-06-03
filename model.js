47782b5671c5816989e5bde7b126816f20d24c7e
samitkumarsinha <samitkumarsinha@gmail.com>
2021-06-03 16:29:07 +0530

    connection

--- model.js
+++ model.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 var uri = "mongodb://127.0.0.1:27017/blog";
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 module.exports = {
     getuserdata(){
-        mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
         var userschema = new mongoose.Schema({
             name: String,
             category: String
@@ -10,7 +10,6 @@ module.exports = {
         return mongoose.models.users || mongoose.model('users', userschema);
     },
     getpostsdata(){
-        mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
         var postschema = new mongoose.Schema({
             title: String,
             body: String,
